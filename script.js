7133ee794d5c02af6fca7a344c7e7ed9e053a50c
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-03-06 12:09:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function shortcut(s1, s2) {
-  // your code here
-}
+    if (!str1 || !str2) return "";
+    return str1[0] + str2[0];}
+
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
